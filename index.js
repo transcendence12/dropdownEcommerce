59daded1b319fd5b99182fac50b6c262d204cab8
Gosia Krawczuk <gosiek.krawczuk@gmail.com>
2022-08-14 20:13:18 +0200

    added constants

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const optionEl = document.querySelectorAll('.option')
 const textBoxEl = document.querySelector('.textBox') //input
-let dropdownEl = document.querySelector('.dropdown')
+const dropdownEl = document.querySelector('.dropdown')
 const selectEl = document.querySelector('.select')
 const optionsEl = document.querySelector('.options')
+const isInStock = document.querySelectorAll('.is-in-stock')
+const infoEls = document.querySelector('.info')
+const sizeEl = document.querySelectorAll('.size')
+console.log(isInStock)
 
 selectEl.onclick = () => {
     dropdownEl.classList.toggle("active")
@@ -10,12 +14,17 @@ selectEl.onclick = () => {
 
 optionEl.forEach((el) => {
     el.addEventListener("click", () => {
+        // isInStock.forEach((i) => {
+        //     i.textContent = ''
+        // })
         textBoxEl.value = el.innerText;
         dropdownEl.classList.remove("active");
         optionEl.forEach((el) => {
             el.classList.remove("selected");
+
         });
         el.classList.add("selected")
+        
     })
 })
 
